1a99d7943ea24356df4b6de3033f06f316fac19e
Nikita Bulatov <87034809+NikBulatov@users.noreply.github.com>
2023-02-15 13:58:06 +0300

    asynchronous functions removed

--- task_1.py
+++ task_1.py
@@ -1,4 +1,3 @@
-import asyncio
 import time
 
 """
@@ -12,30 +11,24 @@ Python 1
 """
 
 
-async def my_fun_1():
-    """Write your code here"""
-    await asyncio.sleep(2)
-    print('2 seconds have passed')
-
-
-async def my_fun_2():
-    """Write your code here"""
-    await asyncio.sleep(5)
-    print('5 seconds have passed')
-
-
-async def main():
-    start = time.time()
+def my_fun_1():
+    '''Write your code here'''
+    time.sleep(2)
+    print ('2 seconds have passed')
     
+    
+def my_fun_2():
+    '''Write your code here'''
+    time.sleep(3)
+    print ('5 seconds have passed')
+    
+    
+def main():
+    start = time.time()
     while True:
-        task_1 = asyncio.create_task(my_fun_1())
-        task_2 = asyncio.create_task(my_fun_2())
+        my_fun_1()
+        my_fun_2()
         
-        for task in task_1, task_2:
-            await task
-            print(time.time() - start, '\n')
-
-
-
+        
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
